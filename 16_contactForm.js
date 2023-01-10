988316d83cd84f67bd93688e83f442d23e214277
Corentin SAMSON <corentin@samson.one>
2023-01-10 10:01:46 +0100

    uptdate 16

--- 16_contactForm.js
+++ 16_contactForm.js
@@ -1,10 +1,12 @@
 const p = document.createElement("p");
 p.id = "result";
+const button = document.getElementsByTagName("button")[0];
+button.insertAdjacentElement("afterend", p);
 const result = document.getElementById("result");
-// const button = document.getElementsByClassName("button");
-// buton.addEventListener("click", () => {
-const yname = document.getElementsByName("name").value;
-const age = document.getElementsByName("age").value;
-const birthdate = document.getElementsByName("birthdate").value;
-result.innerHTML = `name: ${yname}, age: ${age}, birthdate: ${birthdate}`
-// )}
+
+button.addEventListener("click", () => {
+const yname = document.getElementsByName("name")[0].value;
+const age = document.getElementsByName("age")[0].value;
+const birthdate = document.getElementsByName("birthdate")[0].value;
+result.innerHTML = `name: ${yname}, age: ${age}, birthdate: ${birthdate}`;
+})
