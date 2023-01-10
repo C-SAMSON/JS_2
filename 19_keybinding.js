9ea6be9ccf70ce1ebb0609d900e0b48f75ef920a
Corentin SAMSON <corentin@samson.one>
2023-01-10 15:51:41 +0100

    add 19

--- 19_keybinding.js
+++ 19_keybinding.js
@@ -1,6 +1,6 @@
 var square = document.getElementById("square");
 var left = 0;
-var top = 0;
+var topl = 0;
 document.addEventListener("keydown", (e) => {
     switch (e.key) {
         case "ArrowLeft":
@@ -11,13 +11,13 @@ document.addEventListener("keydown", (e) => {
             left += 10;
             square.style.left = left + "px";
             break;
-        case "ArrowDown":
-            top -= 10;
-            square.style.top = top + "px";
-            break;
         case "ArrowUp":
-            top += 10;
-            square.style.top = top + "px";
+            topl -= 10;
+            square.style.top = topl + "px";
+            break;
+        case "ArrowDown":
+            topl += 10;
+            square.style.top = topl + "px";
             break;
     }
 })
