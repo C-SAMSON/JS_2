a6e101fc6806fad7792e0cc7388dc14236315130
Corentin SAMSON <corentin@samson.one>
2023-01-11 10:57:47 +0100

    uptdate

--- 16_contactForm.js
+++ 16_contactForm.js
@@ -5,8 +5,8 @@ button.insertAdjacentElement("afterend", p);
 const result = document.getElementById("result");
 
 button.addEventListener("click", () => {
-const yname = document.getElementsByName("name")[0].value;
-const age = document.getElementsByName("age")[0].value;
-const birthdate = document.getElementsByName("birthdate")[0].value;
-result.innerHTML = `name: ${yname}, age: ${age}, birthdate: ${birthdate}`;
-})
+    const yname = document.getElementsByName("name")[0].value;
+    const age = document.getElementsByName("age")[0].value;
+    const birthdate = document.getElementsByName("birthdate")[0].value;
+    result.innerHTML = `name: ${yname}, age: ${age}, birthdate: ${birthdate}`;
+});
